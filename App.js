8433b2ce4060b83e3af87b8e856c01d6ae0520d1
V0410 <pwlkjakub@gmail.com>
2024-09-16 17:44:15 +0800

    feat: upload profile to ipfs
    Co-Authored-By: lionstar <132696312+lionstar259007@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "@walletconnect/react-native-compat";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, arbitrum } from "@wagmi/core/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createWeb3Modal,
+  defaultWagmiConfig,
+  Web3Modal,
+} from "@web3modal/wagmi-react-native";
+import { Provider } from "react-redux";
+import { store } from "./src/store/store";
+import MainComponent from "./src/components/MainComponent";
+
+// 0. Setup queryClient
+const queryClient = new QueryClient();
+
+// 1. Get projectId at https://cloud.walletconnect.com
+const projectId = process.env.EXPO_PUBLIC_PROJECT_ID;
+
+// 2. Create config
+const metadata = {
+  name: "AppKit RN",
+  description: "AppKit RN Example",
+  url: "https://walletconnect.com",
+  icons: ["https://avatars.githubusercontent.com/u/37784886"],
+  redirect: {
+    native: "YOUR_APP_SCHEME://",
+    universal: "YOUR_APP_UNIVERSAL_LINK.com",
+  },
+};
+
+const chains = [mainnet, polygon, arbitrum];
+
+const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata });
+
+// 3. Create modal
+createWeb3Modal({
+  projectId,
+  wagmiConfig,
+  defaultChain: mainnet, // Optional
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  themeMode: "dark",
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Tomi mesh chhat app</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <Web3Modal />
+          <MainComponent />
+        </Provider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
